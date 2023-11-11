28c6f56957e930980c6452a919e4c9c43687dfdd
Beyza Nur Kaya <95589101+beyz4n@users.noreply.github.com>
2023-11-11 21:18:48 +0300

    Add files via upload

--- Streamlit APP/app.py
+++ Streamlit APP/app.py
@@ -1,26 +1,18 @@
 import streamlit as st
 import numpy as np
 import pickle
+import requests
 
 # Model yükleniyor
-filename = '/Notebooks/titanic_model.sav'
+filename = 'Notebooks/titanic_model.sav'
 model = pickle.load(open(filename, 'rb'))
+
 
 # Eğitim verisinde kullanılan mapping'ler
 title_mapping = {"Bay": 1, "Hanım": 2, "Bayan": 3, "Usta": 4, "Doktor": 5, "Özgü": 6}
 embarked_mapping = {"Southampton, İngiltere": 1, "Cherbourg, Fransa": 2, "Queesntown, İrlanda": 3}
 sex_mapping = {"Erkek": 0, "Kadın": 1}
 ports_range = ('Southampton, İngiltere', 'Cherbourg, Fransa', 'Queesntown, İrlanda')
-
-def prediction(pclass, sex, age, sibsp, parch, embarked, title):
-    new_array = np.array([pclass, sex_mapping[sex], age, sibsp, parch, embarked_mapping[embarked], title_mapping[title]])
-    new_array = new_array.reshape(1, -1)
-    result = model.predict(new_array)
-    proba = model.predict_proba(new_array)
-    if result == 0:
-        return ("Hayatta Kalamazdınız", proba[0][0])
-    else:
-        return ("Hayatta Kalırdınız", proba[0][1])
 
 st.title('Titanik Hayatta Kalma Tahmin Uygulaması')
 
@@ -34,10 +26,18 @@ parch = st.slider('Titanikteki ebeveyn/çocuk sayınız', 0, 6, 0)
 embarked = st.selectbox('Gemiye hangi limandan bindiniz?', ports_range)
 
 # Tahmin
-result = prediction(pclass, sex, age, sibsp, parch, embarked, title)
+response = requests.post("http://fastapi:8000/predict", json={
+        "pclass": pclass,
+        "sex": sex,
+        "title": title,
+        "age": age,
+        "sibsp": sibsp,
+        "parch": parch,
+        "embarked": embarked
+ })
 
-# Sonuçları göster
-if result[0] == 'Hayatta Kalırdınız':
-    st.success(f"%{round(result[1]*100, 2)} ihtimalle {result[0]}")
+res = response.json()
+if res['survive'] == 'Survived':
+    st.success(f"%{round(res['proba'], 2)} ihtimalle hayatta kalırdınız.")
 else:
-    st.error(f"%{round((1-result[1])*100, 2)} ihtimalle {result[0]}")
+    st.error(f"%{round((100 - res['proba']), 2)} ihtimalle hayatta kalmazdınız.")
